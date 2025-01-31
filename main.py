ac61cabb75ed7933da66422b70524c53c9dd3d6d
bdawson0912 <Benjamin.R.Dawson@gmail.com>
2025-01-30 20:48:09 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+import test_libraries
+
+print("test_libraries.py has been executed successfully!")
---
+++ test_libraries.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy as np
+import sqlalchemy
+import psycopg2
+import matplotlib.pyplot as plt
+import seaborn as sns
+import plotly.express as px
+import polars as pl
+
+print("All libraries installed successfully!")
+
